41594a5642e3c21abee859d29622ec9d70430a5d
Nikolai Astakhov <skynetprojectone@gmail.com>
2022-12-28 00:39:48 +0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nikolai Astakhov on 17.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
